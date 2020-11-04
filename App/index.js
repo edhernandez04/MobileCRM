302672f29fc51e3f9af4f13e9964660fe4f47824
edhernandez04 <mchh.004@gmail.com>
2020-11-04 12:07:15 -0500

    login page styled a prepped

--- App/index.js
+++ App/index.js
@@ -8,6 +8,7 @@ import {
   StatusBar,
   Dimensions,
   TextInput,
+  TouchableOpacity,
 } from 'react-native';
 
 const App = () => {
@@ -21,7 +22,11 @@ const App = () => {
         <View style={styles.mainPageContainer}>
           <View style={styles.logoContainer}>
             <Image
-              style={{width: screen.width}}
+              style={{
+                width: screen.width,
+                height: screen.height / 4,
+                justifyContent: 'center',
+              }}
               source={{
                 uri:
                   'https://firebasestorage.googleapis.com/v0/b/mobilecrm-e57c8.appspot.com/o/KeepContact.png?alt=media&token=9df42cb5-57d7-4dd3-a2e9-0fca2fc1816e',
@@ -29,26 +34,40 @@ const App = () => {
             />
           </View>
           <View style={styles.userInfoContainer}>
-            <Text>PLaceholder for UserIcon</Text>
-            <TextInput
-              placeholder="Username"
-              onChangeText={setUsername}
-              value={username}
-            />
-            <Text>PlaceHolder for PasswordIcon</Text>
-            <TextInput
-              placeholder="Password"
-              secureTextEntry={true}
-              onChangeText={setPassword}
-              value={password}
-            />
-            <View style={styles.optionsRow}>
-              <Text>PlaceHolder for Help Button</Text>
-              <Text>PlaceHolder for SignUp Button</Text>
+            <View style={styles.textInputContainer}>
+              <Text>🎃</Text>
+              <TextInput
+                style={styles.inputForm}
+                placeholder="Username"
+                onChangeText={setUsername}
+                value={username}
+              />
             </View>
-          </View>
-          <View style={styles.loginButtonContainer}>
-            <Text>Placeholder for LOGIN button</Text>
+            <View style={styles.textInputContainer}>
+              <Text>🔐</Text>
+              <TextInput
+                style={styles.inputForm}
+                placeholder="Password"
+                secureTextEntry={true}
+                onChangeText={setPassword}
+                value={password}
+              />
+            </View>
+            <View style={styles.optionsRow}>
+              <TouchableOpacity>
+                <Text style={{fontSize: 18}}>⚙️</Text>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <View
+                  style={{
+                    backgroundColor: 'rgba(0, 0, 0, 0.3)',
+                    borderRadius: 25,
+                    padding: 5,
+                  }}>
+                  <Text style={{fontSize: 18, color: 'white'}}>Sign Up ⇢</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
       </SafeAreaView>
@@ -59,22 +78,42 @@ const App = () => {
 const screen = Dimensions.get('window');
 const styles = StyleSheet.create({
   mainPageContainer: {
-    flex: 1,
+    justifyContent: 'space-between',
     height: screen.height,
     width: screen.width,
+    backgroundColor: '#004AAD',
   },
   userInfoContainer: {
-    width: screen.width / 1.3,
-    height: screen.height / 5,
-    justifyContent: 'center',
-    alignItems: 'flex-end',
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    borderRadius: 25,
+    marginBottom: 200,
+    margin: 20,
+    padding: 5,
   },
   logoContainer: {
     width: screen.width,
-    height: screen.height / 5,
-    justifyContent: 'center',
-    alignItems: 'flex-start',
   },
+  textInputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    margin: 5,
+    borderRadius: 25,
+    padding: 10,
+  },
+  inputForm: {
+    height: 50,
+    alignItems: 'center',
+    fontSize: 18,
+    marginLeft: 20,
+  },
+  optionsRow: {
+    margin: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  loginButtonContainer: {},
 });
 
 export default App;
