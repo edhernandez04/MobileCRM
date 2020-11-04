22fe87c3815ac3bdcb3c81ff464a35a401106ce0
edhernandez04 <mchh.004@gmail.com>
2020-11-04 12:38:46 -0500

    styles

--- App/index.js
+++ App/index.js
@@ -14,6 +14,7 @@ import {
 const App = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [signUpVisible, setSignUpVisible] = useState(false);
 
   return (
     <>
@@ -22,17 +23,14 @@ const App = () => {
         <View style={styles.mainPageContainer}>
           <View style={styles.logoContainer}>
             <Image
-              style={{
-                width: screen.width,
-                height: screen.height / 4,
-                justifyContent: 'center',
-              }}
+              style={styles.logo}
               source={{
                 uri:
                   'https://firebasestorage.googleapis.com/v0/b/mobilecrm-e57c8.appspot.com/o/KeepContact.png?alt=media&token=9df42cb5-57d7-4dd3-a2e9-0fca2fc1816e',
               }}
             />
           </View>
+
           <View style={styles.userInfoContainer}>
             <View style={styles.textInputContainer}>
               <Text>🎃</Text>
@@ -43,6 +41,7 @@ const App = () => {
                 value={username}
               />
             </View>
+
             <View style={styles.textInputContainer}>
               <Text>🔐</Text>
               <TextInput
@@ -53,22 +52,19 @@ const App = () => {
                 value={password}
               />
             </View>
+
             <View style={styles.optionsRow}>
               <TouchableOpacity>
-                <Text style={{fontSize: 18}}>⚙️</Text>
+                <Text style={{fontSize: 18}}>Login</Text>
               </TouchableOpacity>
               <TouchableOpacity>
-                <View
-                  style={{
-                    backgroundColor: 'rgba(0, 0, 0, 0.3)',
-                    borderRadius: 25,
-                    padding: 5,
-                  }}>
+                <View style={styles.signUpButton}>
                   <Text style={{fontSize: 18, color: 'white'}}>Sign Up ⇢</Text>
                 </View>
               </TouchableOpacity>
             </View>
           </View>
+
         </View>
       </SafeAreaView>
     </>
@@ -83,8 +79,13 @@ const styles = StyleSheet.create({
     width: screen.width,
     backgroundColor: '#004AAD',
   },
+  logo: {
+    width: screen.width,
+    height: screen.height / 4,
+    justifyContent: 'center',
+  },
   userInfoContainer: {
-    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    backgroundColor: 'rgba(255, 255, 255, 0.4)',
     borderRadius: 25,
     marginBottom: 200,
     margin: 20,
@@ -108,12 +109,20 @@ const styles = StyleSheet.create({
     marginLeft: 20,
   },
   optionsRow: {
-    margin: 10,
+    margin: 20,
     flexDirection: 'row',
-    justifyContent: 'space-around',
+    justifyContent: 'space-between',
     alignItems: 'center',
   },
-  loginButtonContainer: {},
+  loginButtonContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  signUpButton: {
+    backgroundColor: 'rgba(0, 0, 0, 0.3)',
+    borderRadius: 25,
+    padding: 5,
+  },
 });
 
 export default App;
