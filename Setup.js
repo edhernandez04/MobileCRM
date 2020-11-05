f3e4a1895ba2ef47bdca8cc9fcbce8fe92d05808
edhernandez04 <mchh.004@gmail.com>
2020-11-05 14:04:16 -0500

    GoogleSignIn Button 100%

--- Setup.js
+++ Setup.js
@@ -4,9 +4,11 @@ import firebase from '@react-native-firebase/app';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyAmVjAZ_ijMCNNkYj_Zp6sEqZwnCkm5rJk',
-  webClientId: '984461995380-9pfgi65nnch4ijc9sekotrqecqakb5vi.apps.googleusercontent.com',
-  databaseURL: 'https://mobilecrm.firebaseio.com',
-  storageBucket: 'mobilecrm.appspot.com',
+  projectId: 'mobilecrm-e57c8',
+  webClientId:
+    '984461995380-9pfgi65nnch4ijc9sekotrqecqakb5vi.apps.googleusercontent.com',
+  databaseURL: 'https://mobilecrm-e57c8.firebaseio.com',
+  storageBucket: 'mobilecrm-e57c8.appspot.com/',
   appId: '1:984461995380:ios:5e7f8325c22dcfc26e1021',
   messagingSenderId: '984461995380',
   offlineAccess: true,
